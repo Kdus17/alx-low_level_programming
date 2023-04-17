d057c005d6537edeb8afc34f91458771a6b2a91b
kidus <kidus2460@gmail.com>
2023-04-17 11:59:54 -0700

    go

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,30 @@
-julien@ubuntu:~/0x0d. structures, typedef$ cat 0-main.c
-#include <stdio.h>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * dog_t - Typedef for struct dog
  */
-int main(void)
-{
-    struct dog my_dog;
+typedef struct dog dog_t;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
-julien@ubuntu:~/0x0d. structures, typedef$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c -o a
-julien@ubuntu:~/0x0d. structures, typedef$ ./a 
-My name is Poppy, and I am 3.5 :) - Woof!
-julien@ubuntu:~/0x0d. structures, typedef$
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
